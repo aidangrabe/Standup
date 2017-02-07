7a9b5eaa2094148e45dc8d4665d5341d37d05bfc
Aidan Grabe <aidan@teamwork.com>
2017-02-07 20:57:43 +0000

    Show a random color for each item

--- app/src/main/java/com/aidangrabe/standup/rows/LabelRow.kt
+++ app/src/main/java/com/aidangrabe/standup/rows/LabelRow.kt
@@ -1,5 +1,7 @@
 package com.aidangrabe.standup.rows
 
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.TextView
@@ -11,6 +13,17 @@ import com.aidangrabe.standup.list.Row
  */
 class LabelRow(val text: String) : Row {
 
+    companion object {
+        val COLORS = listOf(
+                Color.parseColor("#F44336"),
+                Color.parseColor("#E91E63"),
+                Color.parseColor("#9C27B0"),
+                Color.parseColor("#673AB7"),
+                Color.parseColor("#3F51B5"),
+                Color.parseColor("#2196F3")
+        )
+    }
+
     override fun viewHolderFactory(): (View) -> RecyclerView.ViewHolder {
         return ::ViewHolder
     }
@@ -18,11 +31,23 @@ class LabelRow(val text: String) : Row {
     override fun bind(viewHolder: RecyclerView.ViewHolder) {
         with (viewHolder as ViewHolder) {
             label.text = text
-            characterLabel.text = text.substring(0, 1).toUpperCase()
+            val firstLetter = Character.toUpperCase(text[0])
+            characterLabel.text = firstLetter.toString()
+            tintLetterBackground(characterLabel, firstLetter)
         }
     }
 
     override fun viewId() = R.layout.row_label
+
+    private fun tintLetterBackground(textView: TextView, firstLetter: Char) {
+        val background = textView.background
+        background?.let {
+            if (it is GradientDrawable) {
+                val letterNum = firstLetter.toInt()
+                it.setColor(COLORS[letterNum % COLORS.size])
+            }
+        }
+    }
 
     private class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
